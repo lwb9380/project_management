90dd7418d9af182348d9faf80daaaea5af90af03
ohyoubee <dhdbql1022@gmail.com>
2023-10-04 16:34:08 +0900

    2023-10-04 16:33
    오유비
    ------- 수정 사항 -------
    isworking
    기존 - 출근전,출근,퇴근
    추가 - 외출,휴가

--- src/main/java/com/pro/project/controller/DeptController.java
+++ src/main/java/com/pro/project/controller/DeptController.java
@@ -15,15 +15,19 @@ public class DeptController {
 
     @GetMapping("ex2")
     public String deptList(@RequestParam(name = "deptno", defaultValue = "1") Long deptno, Model model) {
-        Long preWorkCount = deptService.countPreWorkByDeptNo(deptno);  //카운트
-        Long workCount = deptService.countWorkByDeptNo(deptno); //카운트
-        Long leaveCount = deptService.countLeaveByDeptNo(deptno); //카운트
+        Long preWorkCount = deptService.countPreWorkByDeptNo(deptno);  //출근전 카운트
+        Long workCount = deptService.countWorkByDeptNo(deptno); //출근 카운트
+        Long leaveCount = deptService.countLeaveByDeptNo(deptno); //퇴근 카운트
+        Long commuteCount = deptService.countCommuteByDeptNo(deptno); //외출 카운트
+        Long vacationCount = deptService.countVacationByDeptNo(deptno); //휴가 카운트
 
         String deptname = deptService.getDeptnameByDeptno(deptno); //이름가져오기
 
         model.addAttribute("preWorkCount", preWorkCount);
         model.addAttribute("workCount", workCount);
         model.addAttribute("leaveCount", leaveCount);
+        model.addAttribute("commuteCount", commuteCount);
+        model.addAttribute("vacationCount", vacationCount);
         model.addAttribute("deptname", deptname);
 
         return "ex2";
--- src/main/java/com/pro/project/service/DeptService.java
+++ src/main/java/com/pro/project/service/DeptService.java
@@ -10,17 +10,25 @@ public class DeptService {
     @Autowired
     DeptRepository deptRepository;
 
-
+    //출근전
     public Long countPreWorkByDeptNo(Long deptno) {
         return deptRepository.countPreWork(deptno);
     }
-
+    //출근
     public Long countWorkByDeptNo(Long deptno) {
         return deptRepository.countWork(deptno);
     }
-
+    //퇴근
     public Long countLeaveByDeptNo(Long deptno) {
         return deptRepository.countLeave(deptno);
+    }
+    //외출
+    public Long countCommuteByDeptNo(Long deptno) {
+        return deptRepository.countCommute(deptno);
+    }
+    //휴가
+    public Long countVacationByDeptNo(Long deptno) {
+        return deptRepository.countVacation(deptno);
     }
 
     public String getDeptnameByDeptno(Long deptno) {
