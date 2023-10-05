9f5766950fbf710bb771e9fd5e3f04562973684e
KingOfMockdong <jominsu0520@gmail.com>
2023-10-05 17:23:57 +0900

    로그인된 상태에서 주소창에 로그인주소 입력해도 로그인페이지로 안가지게 수정함.

--- src/main/java/com/pro/project/controller/LoginController.java
+++ src/main/java/com/pro/project/controller/LoginController.java
@@ -5,12 +5,10 @@ import com.pro.project.spring.WrongIdPasswordException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 
@@ -19,29 +17,31 @@ public class LoginController {
 
     @Autowired
     private AuthService authService;
-//    public void setAuthService(AuthService authService) {
-//        this.authService = authService;
-//    }
 
     @GetMapping("/login")
-    public String form(LoginCommand loginCommand,
-                       @CookieValue(value = "dog", required = false) Cookie rCookie) {
-        System.out.println(rCookie);
-        if (rCookie != null) {
-            loginCommand.setEmpno(Long.valueOf(rCookie.getValue()));
-            loginCommand.isRememberEmpno(true);
-        }
+    public String form(LoginCommand loginCommand, HttpSession session) {
         if (loginCommand == null) {
             loginCommand = new LoginCommand();
         }
-
+        // 로그인된 사용자의 경우 메인 페이지로 리다이렉트
+//        if (!(boolean)session.getAttribute("authInfo")) {
+//            return "login/loginForm";
+//        } else {
+//            return "login/loginSuccess";
+//        }
+        if(session.getAttribute("authInfo")!=null) {
+            if((boolean) session.getAttribute("authInfo")){
+                return "login/loginSuccess";
+            }
+            return "login/loginForm";
+        }
         return "login/loginForm";
     }
 
+
     @PostMapping("/login")
     public String submit(
-            LoginCommand loginCommand, Errors errors, HttpSession session,
-            HttpServletResponse response) {
+            LoginCommand loginCommand, Errors errors, HttpSession session, HttpServletRequest request) {
         new LoginCommandValidator().validate(loginCommand, errors);
 
         if (errors.hasErrors()) {
@@ -51,30 +51,15 @@ public class LoginController {
             boolean authInfo = authService.authenticate(
                     loginCommand.getEmpno(),
                     loginCommand.getPassword());
-
-            session.setAttribute("authInfo", authInfo); // 로그인을 성공하면 인증정보 객체를 저장
-
-            // 쿠키의 유효기간 설정
-            Cookie rememberCookie = new Cookie("dog", "" + loginCommand.getEmpno());
-            System.out.println(rememberCookie);
-            rememberCookie.setPath("/");
-            if (loginCommand.isRememberEmpno(loginCommand.getRememberEmpno())) {
-                // 30일 동안 쿠키를 유지하도록 설정
-                rememberCookie.setMaxAge(60 * 60 * 24 * 30);
-            } else {
-                // 쿠키를 세션 기간 동안 유지하도록 설정
-                rememberCookie.setMaxAge(0);
-            }
-            response.addCookie(rememberCookie);
-
+            session.setAttribute("authInfo", authInfo);
             session.setAttribute("user", loginCommand.getEmpno());
-            session.setAttribute("password", loginCommand.getPassword());
 
             return "login/loginSuccess";
+
         } catch (WrongIdPasswordException e) {
             errors.reject("idPasswordNotMatching");
             return "login/loginForm";
         }
-    }
 
-}
+    }
+    }
