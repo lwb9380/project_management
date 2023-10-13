59f6374c948e91996b423b7055e72f47c4554104
Taewoong-Kim <hiber_01@naver.com>
2023-10-13 17:12:03 +0900

    휴가 신청 폼 css 수정 및 registerpage css 추가

--- src/main/java/com/pro/project/repository/VacationRequestRepository.java
+++ src/main/java/com/pro/project/repository/VacationRequestRepository.java
@@ -4,8 +4,10 @@ package com.pro.project.repository;
 import com.pro.project.entity.Emp;
 import com.pro.project.entity.VacationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VacationRequestRepository extends JpaRepository<VacationRequest, Long> {
-
+    Page<VacationRequest> findAll(Pageable pageable);
 }
